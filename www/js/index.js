95927da223a79f3482b2ab50a484c0427547e93a
Sy <ousy@et.esiea.fr>
2023-06-19 22:13:53 +0200

    Ajout des documents

---
+++ www/js/index.js
@@ -0,0 +1,65 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+// Wait for the deviceready event before using any of Cordova's device APIs.
+// See https://cordova.apache.org/docs/en/latest/cordova/events/events.html#deviceready
+//document.addEventListener('deviceready', onDeviceReady, false);
+document.addEventListener('deviceready', onDeviceReady, false);
+var myapp
+function onDeviceReady() {
+    // Cordova is now initialized. Have fun!
+
+    myapp = Vue.createApp({
+        data(){
+            return{
+                message: 'Liste de Pokémons',
+                isTrue : true,
+                isFalse : false,
+                listItems: [],
+                Description : [],
+                offset:0
+            }
+        },
+        methods: {
+            async getData() {
+                fetch(`https://pokeapi.co/api/v2/pokemon/?offset=${this.offset}&limit='20'`)
+                .then(res => res.json())
+                .then(finalRes => {
+                this.listItems = finalRes.results;
+                finalRes.results.forEach(pokemon =>{
+                    fetch(pokemon.url)
+                    .then(res2 => res2.json())
+                    .then(finalRes2 => {
+                        this.Description = finalRes2.stats
+                    })
+                })
+                });
+              },
+              
+            async get10nextPokemons(){
+                this.offset += 10;
+                await this.getData();
+            },
+        },
+        mounted() {
+            this.getData()
+          }
+    })
+    myapp.mount('#app')
+}
